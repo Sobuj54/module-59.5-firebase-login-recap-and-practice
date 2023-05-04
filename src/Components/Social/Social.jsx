911de4bc0a658a3b2ad2233817b5d2ea819a2997
soubj <sobujahmed124@gmail.com>
2023-05-04 16:31:18 +0600

    created basic routes

---
+++ src/Components/Social/Social.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div>
+      <h2>social login page</h2>
+    </div>
+  );
+};
+
+export default Social;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./Components/Layout/Main.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Social from "./Components/Social/Social.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/social",
+        element: <Social></Social>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
