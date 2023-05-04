092fafe660ad236197d775b5b25cd5e0b5ba0031
soubj <sobujahmed124@gmail.com>
2023-05-04 16:19:29 +0600

    started new firebase practice project

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPvTUwmX0X8BMY3EZYywgwGKHjFVeuJWM",
+  authDomain: "fir-login-recap.firebaseapp.com",
+  projectId: "fir-login-recap",
+  storageBucket: "fir-login-recap.appspot.com",
+  messagingSenderId: "1049699052300",
+  appId: "1:1049699052300:web:6885f6ebc6971f31a37729",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
